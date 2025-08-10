de028b58a86b52b782562b90a90823f0b8931bc0
packedcrack <52038722+PackedCrack@users.noreply.github.com>
2025-08-10 16:24:37 +0200

    Removed UNHANDLED_SWITCH_CASE define. (forgot to include the changes in last push lmfao.)

--- src/Debugging/src/debug_defines.h
+++ src/Debugging/src/debug_defines.h
@@ -8,12 +8,8 @@ inline void debugbreak()
     __asm __volatile__("int3");
 }
     #define DEBUG_BREAK debugbreak()
-    #define UNHANDLED_CASE_PROTECTION_ON _Pragma("clang diagnostic error \"-Wswitch-enum\"")
-    #define UNHANDLED_CASE_PROTECTION_OFF _Pragma("clang diagnostic warning \"-Wswitch-enum\"")
 #elif _MSC_VER
     #define DEBUG_BREAK __debugbreak()
-    #define UNHANDLED_CASE_PROTECTION_ON __pragma(warning(error : Wswitch))
-    #define UNHANDLED_CASE_PROTECTION_OFF __pragma(warning(default : Wswitch))
 #else
     #error Unsupported compiler
 #endif
