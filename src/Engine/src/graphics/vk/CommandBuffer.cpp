df89e394562b8b0f8e4824e58f12029fa1fb2f27
packedcrack <52038722+PackedCrack@users.noreply.github.com>
2025-08-15 22:24:36 +0200

    CommandBuffer and CommandPool RAII wrappers.

---
+++ src/Engine/src/graphics/vk/CommandBuffer.cpp
@@ -0,0 +1,61 @@
+#include "CommandBuffer.hpp"
+
+#include "vulkan_defines.hpp"
+//
+//
+namespace odin::graphics::vk
+{
+CommandBuffer::CommandBuffer(VkCommandBuffer cmdBuffer)
+	: m_CommandBuffer{ cmdBuffer }
+{
+	ODIN_ASSERT(cmdBuffer != VK_NULL_HANDLE);
+}
+CommandBuffer::CommandBuffer(const CommandBuffer& other) noexcept
+	: m_CommandBuffer{ std::exchange(other.m_CommandBuffer, m_CommandBuffer) }
+{}
+CommandBuffer::CommandBuffer(CommandBuffer&& other) noexcept
+	: m_CommandBuffer{ std::exchange(other.m_CommandBuffer, m_CommandBuffer) }
+{}
+CommandBuffer& CommandBuffer::operator=(const CommandBuffer& other) noexcept
+{
+	if (this != std::addressof(other))
+	{
+		m_CommandBuffer = std::exchange(other.m_CommandBuffer, m_CommandBuffer);
+	}
+
+	return *this;
+}
+CommandBuffer& CommandBuffer::operator=(CommandBuffer&& other) noexcept
+{
+	if (this != std::addressof(other))
+	{
+		m_CommandBuffer = std::exchange(other.m_CommandBuffer, m_CommandBuffer);
+	}
+
+	return *this;
+}
+void CommandBuffer::reset()
+{
+	VK_CHECK(vkResetCommandBuffer(m_CommandBuffer, VK_NO_FLAGS), "Failed to reset Vulkan Command Buffer.");
+}
+void CommandBuffer::begin()
+{
+	static constexpr VkCommandBufferBeginInfo info =
+	{
+		.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+		.pNext = nullptr,
+		.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
+		.pInheritanceInfo = nullptr
+	};
+	VK_CHECK(vkBeginCommandBuffer(m_CommandBuffer, std::addressof(info)), "Failed to Begin Command Buffer.");
+}
+void CommandBuffer::end()
+{
+	VK_CHECK(vkEndCommandBuffer(m_CommandBuffer), "Failed to End Command Buffer.");
+}
+CommandBufferRef CommandBuffer::handle() const
+{
+	ODIN_ASSERT(m_CommandBuffer != VK_NULL_HANDLE);
+	return CommandBufferRef{ .handle = m_CommandBuffer };
+}
+}	// namespace odin::graphics::vk
---
+++ src/Engine/src/graphics/vk/CommandBuffer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CommandPool.hpp"
+#include "Device.hpp"
+//
+//
+namespace odin::graphics::vk
+{
+struct CommandBufferRef
+{
+	VkCommandBuffer handle;
+};
+class CommandBuffer
+{
+public:
+	CommandBuffer(VkCommandBuffer cmdBuffer);
+	~CommandBuffer() = default;
+	CommandBuffer(const CommandBuffer& other);
+	CommandBuffer(CommandBuffer&& other) noexcept;
+	CommandBuffer& operator=(const CommandBuffer& other);
+	CommandBuffer& operator=(CommandBuffer&& other) noexcept;
+public:
+	void reset();
+	void begin();
+	void end();
+	[[nodiscard]] CommandBufferRef handle() const;
+private:
+	VkCommandBuffer m_CommandBuffer = VK_NULL_HANDLE;
+};
+}	// namespace odin::graphics::vk
