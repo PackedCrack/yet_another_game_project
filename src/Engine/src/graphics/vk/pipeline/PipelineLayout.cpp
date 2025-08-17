fb974376fea75eb365a2983018821982c48c3424
packedcrack <52038722+PackedCrack@users.noreply.github.com>
2025-08-17 20:54:09 +0200

    Added pipelinelayout RAII wrapper

---
+++ src/Engine/src/graphics/vk/pipeline/PipelineLayout.cpp
@@ -0,0 +1,54 @@
+#include "PipelineLayout.hpp"
+
+#include "../vulkan_defines.hpp"
+#include "../vulkan_info.hpp"
+//
+//
+namespace odin::graphics::vk::pipeline
+{
+PipelineLayout::PipelineLayout(DeviceRef device, 
+							   const std::vector<VkDescriptorSetLayout>& descriptorLayouts, 
+							   std::optional<std::reference_wrapper<std::vector<VkPushConstantRange>>> pushContantRanges)
+	: m_Layout{ VK_NULL_HANDLE }
+	, m_Device{ device }
+{
+	std::vector<VkPushConstantRange> ranges{};
+	if (pushContantRanges)
+	{
+		// Just copy.. how often would this really be called?
+		ranges = pushContantRanges->get();
+	}
+
+	VkPipelineLayoutCreateInfo info = pipeline_layout_create_info(ranges, descriptorLayouts);
+	VK_CHECK(vkCreatePipelineLayout(m_Device.handle, std::addressof(info), nullptr, std::addressof(m_Layout)),
+			 "Failed to create Vulkan Pipeline Layout");
+}
+PipelineLayout::~PipelineLayout()
+{
+	if (m_Layout != VK_NULL_HANDLE)
+	{
+		vkDestroyPipelineLayout(m_Device.handle, m_Layout, nullptr);
+	}
+}
+PipelineLayout::PipelineLayout(PipelineLayout&& other) noexcept
+	: m_Layout{ VK_NULL_HANDLE }
+	, m_Device{ other.m_Device }
+{
+	std::swap(m_Layout, other.m_Layout);
+}
+PipelineLayout& PipelineLayout::operator=(PipelineLayout&& other) noexcept
+{
+	if (this != std::addressof(other))
+	{
+		std::swap(m_Layout, other.m_Layout);
+		m_Device = other.m_Device;
+	}
+
+	return *this;
+}
+PipelineLayoutRef PipelineLayout::handle() const
+{
+	ODIN_ASSERT(m_Layout != VK_NULL_HANDLE);
+	return PipelineLayoutRef{ .handle = m_Layout };
+}
+}	// namespace odin::graphics::vk::pipeline
---
+++ src/Engine/src/graphics/vk/pipeline/PipelineLayout.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../Device.hpp"
+// vulkan
+#include <vulkan/vulkan.h>
+//
+//
+namespace odin::graphics::vk::pipeline
+{
+struct PipelineLayoutRef
+{
+	VkPipelineLayout handle;
+};
+class PipelineLayout
+{
+public:
+	PipelineLayout(DeviceRef device, const std::vector<VkDescriptorSetLayout>& descriptorLayouts, std::optional<std::reference_wrapper<std::vector<VkPushConstantRange>>> pushContantRanges = std::nullopt);
+	~PipelineLayout();
+	PipelineLayout(const PipelineLayout& other) = delete;
+	PipelineLayout(PipelineLayout&& other) noexcept;
+	PipelineLayout& operator=(const PipelineLayout& other) = delete;
+	PipelineLayout& operator=(PipelineLayout&& other) noexcept;
+public:
+	[[nodiscard]] PipelineLayoutRef handle() const;
+private:
+	VkPipelineLayout m_Layout = VK_NULL_HANDLE;
+	DeviceRef m_Device;
+};
+}	// namespace odin::graphics::vk::pipeline
