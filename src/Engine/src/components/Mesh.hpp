87f7041737d93590ace02ee4e13b8a7ba955638d
packedcrack <52038722+PackedCrack@users.noreply.github.com>
2025-08-23 20:23:04 +0200

    Added components: LocalTRS, WorldTRS, Mesh, Model, Parent

---
+++ src/Engine/src/components/Mesh.hpp
@@ -0,0 +1,17 @@
+//
+// Created by qwerty on 22/08/2025.
+//
+#pragma once
+
+// std
+#include <cstdint>
+//
+//
+namespace odin::components
+{
+using MeshID = std::int32_t;
+struct Mesh
+{
+    MeshID id;
+};
+}    // namespace odin::components
