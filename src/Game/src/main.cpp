5c9a6b4c6409b927ed13aadefa769dd264c07bb5
packedcrack <52038722+PackedCrack@users.noreply.github.com>
2025-11-16 15:04:30 +0100

    Added functions to Odin.cpp to calculate the WorldTRS of entites.
    Odin.cpp's make_submesh_entites now creates the expected root and node entities that component::Model expects.
    
    Example translate/rotate code in main.cpp

--- src/Game/src/main.cpp
+++ src/Game/src/main.cpp
@@ -6,6 +6,8 @@
 // Enginge
 #include <engine/Odin.hpp>
 #include <engine/components/Model.hpp>
+#include <engine/components/Geometry.hpp>
+#include <engine/components/Parent.hpp>
 #include <engine/state/Input.hpp>
 #include <engine/window/DisplayResolution.hpp>
 // Debug
@@ -24,15 +26,40 @@ int main(int argc, char** argv)
         odin::OdinInfo info{ .argc = argc, .argv = argv, .applicationName = "Odin Application", .windowInfo = std::move(wndInfo) };
         odin::Odin engine{ info };
 
-        std::unique_ptr<odin::ECS> ecs = engine.make_ecs();
-        odin::Entity e = ecs->make_entity();
-        std::filesystem::path filepath{ R"(C:\Users\qwerty\Documents\repos\game\resources\assets\meshes\Lantern.glb)" };
-        e.emplace<odin::component::Model>(filepath);
 
+        odin::Entity e = odin::make_entity();
+        std::filesystem::path filepath{ R"(C:\Users\qwerty\Documents\repos\game\resources\assets\meshes\Lantern.glb)" };
+        odin::emplace_component<odin::component::Model>(e, filepath);
+
+        auto move = [&e]()
+        {
+            const auto& model = odin::get_components<odin::component::Model>(e);
+            static bool a = true;
+            if (a)
+            {
+                odin::rotate(model.root, 90.0f, { 1.0f, 0.0f, 1.0f });
+                a = false;
+            }
+
+            auto time = (float) std::chrono::steady_clock::now().time_since_epoch().count();
+            //odin::rotate(model.root, 0.01f, { 0.0f, 1.0f, 0.0f });
+            for (auto&& node : model.nodes)
+            {
+                glm::vec3 delta{};
+                delta.y = std::sin(time) / 10;
+                //delta.x = std::sin(time) / 10;
+
+                const odin::Entity& n = node.second;
+                odin::translate(n, delta);
+            }
+        };
 
         while (engine.running())
         {
             engine.begin_frame();
+
+            move();
+
             engine.render();
             engine.end_frame();
         }
